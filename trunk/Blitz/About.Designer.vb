e5af90d0b1b094511dac30897a9110fa002996b9
psykad <psykad@6253fb84-a158-affe-742d-674e0bb33499>
2010-05-06 09:22:36 +0000

    Initial commit rev 413

---
+++ trunk/Blitz/About.Designer.vb
@@ -0,0 +1,94 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class About
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblAppName = New System.Windows.Forms.Label
+        Me.lblAppAuthor = New System.Windows.Forms.Label
+        Me.lblAppVersion = New System.Windows.Forms.Label
+        Me.btnOK = New System.Windows.Forms.Button
+        Me.SuspendLayout()
+        '
+        'lblAppName
+        '
+        Me.lblAppName.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAppName.Location = New System.Drawing.Point(12, 9)
+        Me.lblAppName.Name = "lblAppName"
+        Me.lblAppName.Size = New System.Drawing.Size(216, 47)
+        Me.lblAppName.TabIndex = 0
+        Me.lblAppName.Text = "App Name"
+        Me.lblAppName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblAppAuthor
+        '
+        Me.lblAppAuthor.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAppAuthor.ForeColor = System.Drawing.Color.Green
+        Me.lblAppAuthor.Location = New System.Drawing.Point(12, 56)
+        Me.lblAppAuthor.Name = "lblAppAuthor"
+        Me.lblAppAuthor.Size = New System.Drawing.Size(216, 23)
+        Me.lblAppAuthor.TabIndex = 1
+        Me.lblAppAuthor.Text = "App Author"
+        Me.lblAppAuthor.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblAppVersion
+        '
+        Me.lblAppVersion.Location = New System.Drawing.Point(16, 86)
+        Me.lblAppVersion.Name = "lblAppVersion"
+        Me.lblAppVersion.Size = New System.Drawing.Size(212, 23)
+        Me.lblAppVersion.TabIndex = 2
+        Me.lblAppVersion.Text = "App Version"
+        Me.lblAppVersion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnOK
+        '
+        Me.btnOK.Location = New System.Drawing.Point(84, 128)
+        Me.btnOK.Name = "btnOK"
+        Me.btnOK.Size = New System.Drawing.Size(75, 23)
+        Me.btnOK.TabIndex = 3
+        Me.btnOK.Text = "OK"
+        Me.btnOK.UseVisualStyleBackColor = True
+        '
+        'About
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(240, 163)
+        Me.Controls.Add(Me.btnOK)
+        Me.Controls.Add(Me.lblAppVersion)
+        Me.Controls.Add(Me.lblAppAuthor)
+        Me.Controls.Add(Me.lblAppName)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "About"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "About"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents lblAppName As System.Windows.Forms.Label
+    Friend WithEvents lblAppAuthor As System.Windows.Forms.Label
+    Friend WithEvents lblAppVersion As System.Windows.Forms.Label
+    Friend WithEvents btnOK As System.Windows.Forms.Button
+End Class
---
+++ trunk/Blitz/About.vb
@@ -0,0 +1,30 @@
+﻿'    Copyright (C) 2009-2010  Ryan Skeldon <psykad@gmail.com>
+'
+'    This program is free software; you can redistribute it and/or modify
+'    it under the terms of the GNU General Public License as published by
+'    the Free Software Foundation; either version 2 of the License, or
+'    (at your option) any later version.
+'
+'    This program is distributed in the hope that it will be useful,
+'    but WITHOUT ANY WARRANTY; without even the implied warranty of
+'    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'    GNU General Public License for more details.
+'
+'    You should have received a copy of the GNU General Public License
+'    along with this program; if not, write to the Free Software
+'    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+Option Explicit On
+
+Public Class About
+    Private Sub About_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim VersionInfo As FileVersionInfo = FileVersionInfo.GetVersionInfo(Application.ExecutablePath)
+
+        lblAppName.Text = Application.ProductName
+        lblAppAuthor.Text = "by Ryan Skeldon"
+        lblAppVersion.Text = "Version " & VersionInfo.ProductVersion
+    End Sub
+
+    Private Sub btnOK_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnOK.Click
+        Me.Close()
+    End Sub
+End Class
