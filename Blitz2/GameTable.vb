59cd0f3b793ffd7720b2c3bb0d6a72148b3ba50e
psykad <psykad@6253fb84-a158-affe-742d-674e0bb33499>
2010-08-23 08:29:41 +0000

    [Blitz2]
    Added Card and Player class files. Basic graphics coded. Updated BlitzCards.dll added.

--- Blitz2/GameTable.vb
+++ Blitz2/GameTable.vb
@@ -1,3 +1,317 @@
-﻿Public Class GameTable
+﻿'    Blitz, a simple draw and discard game, suitable for players of all ages.
+'    Copyright (C) 2009-2010  Ryan Skeldon <psykad@gmail.com>
+'
+'    This program is free software; you can redistribute it and/or modify
+'    it under the terms of the GNU General Public License as published by
+'    the Free Software Foundation; either version 2 of the License, or
+'    (at your option) any later version.
+'
+'    This program is distributed in the hope that it will be useful,
+'    but WITHOUT ANY WARRANTY; without even the implied warranty of
+'    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'    GNU General Public License for more details.
+'
+'    You should have received a copy of the GNU General Public License
+'    along with this program; if not, write to the Free Software
+'    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+Option Explicit On
+Imports System.Threading
+Imports Blitz2.Objects
+Imports Blitz2.Objects.Card
+
+Public Class GameTable
+#Region "Game Fields"
+    Private deck(51) As Card
+    Public Player(5) As Player
+
+    Private seed As Integer
+
+    Private gameActive As Boolean
+    Private roundActive As Boolean
+
+    Private currentPlayer As Byte
+    Private knocker As Byte
+    Private winner As Byte
+    Private dealer As Byte
+
+    Private discardTop As Byte
+    Private discardBottom As Byte
+    Private discardCount As Byte
+
+    Private cardOffsetX As Byte = 16
+    Private cardOffsetY As Byte = 30
+
+    Private Const noCard As Byte = 52
+
+    Private takingTurn As Boolean
+    Private computerSync As New Object
+    Private computerThread As Thread
+
+    Private Enum CardOwners
+        Deck = 0
+        Discard = 5
+    End Enum
+#End Region
+
+#Region "Form Methods"
+    Public Sub New()
+        ' This call is required by the Windows Form Designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        Me.SetStyle(ControlStyles.DoubleBuffer Or ControlStyles.UserPaint Or ControlStyles.AllPaintingInWmPaint, True)
+        Me.UpdateStyles()
+    End Sub
+
+    Private Sub GameTable_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Try
+            If Not Card.Initialize Then Exit Sub
+        Catch ex As Exception
+            MsgBox("Unable to load card library.", MsgBoxStyle.Critical + MsgBoxStyle.OkOnly, "Card Library Error")
+            Exit Sub
+        End Try
+
+#If DEBUG Then
+        cardOffsetX = Card.CardWidth + 10
+        cardOffsetY = Card.CardHeight + 10
+#End If
+
+        CreatePlayers()
+    End Sub
+
+    Private Sub GameTable_Closing() Handles Me.Closing
+
+    End Sub
+
+    Private Sub GameTable_Paint(ByVal sender As Object, ByVal e As System.Windows.Forms.PaintEventArgs) Handles Me.Paint
+        Dim i As Byte
+
+        If Not gameActive Then Exit Sub
+
+        SetCardLocations()
+
+        For i = 0 To UBound(deck)
+            Select Case deck(i).Owner
+                Case 1
+                    With Player(1)
+                        If .InGame Then
+                            PaintCard(e.Graphics, New Point(.HandLocation.X, .HandLocation.Y), i, deck(i).Invert)
+                            .HandLocation = New Point(.HandLocation.X + cardOffsetX, .HandLocation.Y)
+                        End If
+                    End With
+                Case 2
+                    With Player(2)
+                        If .InGame Then
+                            If Not roundActive Then
+                                PaintCard(e.Graphics, New Point(.HandLocation.X, .HandLocation.Y), i, False)
+                            Else
+                                PaintCard(e.Graphics, New Point(.HandLocation.X, .HandLocation.Y), noCard, False)
+                            End If
+                            .HandLocation = New Point(.HandLocation.X, .HandLocation.Y + cardOffsetY)
+                        End If
+                    End With
+                Case 3
+                    With Player(3)
+                        If .InGame Then
+                            If Not roundActive Then
+                                PaintCard(e.Graphics, New Point(.HandLocation.X, .HandLocation.Y), i, False)
+                            Else
+                                PaintCard(e.Graphics, New Point(.HandLocation.X, .HandLocation.Y), noCard, False)
+                            End If
+                            .HandLocation = New Point(.HandLocation.X + cardOffsetX, .HandLocation.Y)
+                        End If
+                    End With
+                Case 4
+                    With Player(4)
+                        If .InGame Then
+                            If Not roundActive Then
+                                PaintCard(e.Graphics, New Point(.HandLocation.X, .HandLocation.Y), i, False)
+                            Else
+                                PaintCard(e.Graphics, New Point(.HandLocation.X, .HandLocation.Y), noCard, False)
+                            End If
+                            .HandLocation = New Point(.HandLocation.X, .HandLocation.Y + cardOffsetY)
+                        End If
+                    End With
+            End Select
+        Next
+
+        PaintCard(e.Graphics, New Point(Player(CardOwners.Deck).HandLocation.X, Player(CardOwners.Deck).HandLocation.Y), noCard, False)
+
+        If discardTop <> noCard Then
+            PaintCard(e.Graphics, New Point(Player(CardOwners.Discard).HandLocation.X, Player(CardOwners.Discard).HandLocation.Y), discardTop, deck(discardTop).Invert)
+        End If
+
+        Me.Refresh()
+    End Sub
+#End Region
+
+#Region "Game Methods"
+    Private Sub NewGame()
+        Dim i As Byte
+
+        Cursor.Current = Cursors.WaitCursor
+
+        SyncLock computerSync
+            If takingTurn Then
+                Try
+                    computerThread.Abort()
+                Catch ex As Exception
+
+                End Try
+            End If
+        End SyncLock
+
+        gameActive = False
+        roundActive = False
+
+        For i = 1 To 4
+            Me.Player(i).Tokens = 4
+            Me.Player(i).InGame = True
+        Next
+
+        gameActive = True
+        Cursor.Current = Cursors.Arrow
+
+        NewRound()
+    End Sub
+
+    Private Sub NewRound()
+        Dim rnd As New Random
+        Dim i As Byte
+
+        Cursor.Current = Cursors.WaitCursor
+
+        seed = rnd.Next(0, 65535)
+
+        For i = 0 To 4
+            Player(i).CreateNewHand()
+            Player(i).Flagged = False
+        Next
+
+        roundActive = True
+
+        ResetDeck()
+        DealCards()
+        Me.Refresh()
+        Cursor.Current = Cursors.Arrow
+    End Sub
+#End Region
+
+#Region "Card Methods"
+    Private Sub DealCards()
+        Dim i As Byte
+        Dim player As Byte = Me.dealer + 1
+
+        For i = 1 To 12
+            If player > 4 Then player = 1
+
+            If Me.Player(player).InGame Then
+                MoveCard(CardOwners.Deck, player)
+                'Me.Refresh()
+            End If
+
+            player += 1
+        Next
+
+        MoveCard(CardOwners.Deck, CardOwners.Discard)
+        discardCount = 1
+        discardBottom = noCard
+    End Sub
+
+    Private Sub MoveCard(ByVal fromPlayer As Byte, ByVal toPlayer As Byte, Optional ByVal card As Byte = nocard)
+        Select Case fromPlayer
+            Case CardOwners.Deck
+                Dim cardFound As Boolean
+                Dim rnd As New Random(seed)
+
+                Do While Not cardFound
+                    card = rnd.Next(0, 52)
+
+                    If deck(card).Owner = CardOwners.Deck Then
+                        deck(card).Owner = toPlayer
+
+                        Select Case toPlayer
+                            Case 1, 2, 3, 4
+                                Player(toPlayer).AddCard(card)
+                            Case CardOwners.Discard
+                                discardTop = card
+                        End Select
+                        cardFound = True
+                    End If
+                Loop
+            Case CardOwners.Discard
+
+            Case Else
+
+        End Select
+    End Sub
+
+    Private Sub ResetDeck()
+        Dim i As Byte
+
+        For i = 0 To UBound(deck)
+            deck(i) = New Card(i)
+        Next
+
+        discardTop = noCard
+        discardBottom = noCard
+        discardCount = 0
+    End Sub
+#End Region
+
+#Region "Player Methods"
+    Private Sub CreatePlayers()
+        Dim i As Byte
+
+        For i = 0 To UBound(Me.Player)
+            Me.Player(i) = New Player(Objects.Player.Modes.Computer)
+        Next
+
+        Me.Player(1).Mode = Objects.Player.Modes.Human
+
+        With Me
+            .Player(1).Name = "Player 1"
+            .Player(2).Name = "Player 2"
+            .Player(3).Name = "Player 3"
+            .Player(4).Name = "Player 4"
+
+            .Player(CardOwners.Deck).HandLocation = New Point((290 - Card.CardWidth), (300 - (Card.CardHeight / 2)))
+            .Player(CardOwners.Discard).HandLocation = New Point(310, (300 - (Card.CardHeight / 2)))
+            .Player(1).HandLocationMid = New Point(300, 500)
+            .Player(2).HandLocationMid = New Point(100, 300)
+            .Player(3).HandLocationMid = New Point(300, 100)
+            .Player(4).HandLocationMid = New Point(500, 300)
+        End With
+    End Sub
+
+    Private Sub SetCardLocations()
+        With Player(1)
+            .HandLocation = New Point(.HandLocationMid.X - (Card.CardWidth + (cardOffsetX * (.TotalCards - 1))) / 2, _
+                                      .HandLocationMid.Y - (Card.CardHeight / 2))
+        End With
+        With Player(2)
+            .HandLocation = New Point(.HandLocationMid.X - (Card.CardWidth / 2), _
+                                      .HandLocationMid.Y - (Card.CardHeight + cardOffsetY * (.TotalCards - 1)) / 2)
+        End With
+        With Player(3)
+            .HandLocation = New Point(.HandLocationMid.X - ((Card.CardWidth + cardOffsetX * (.TotalCards - 1)) / 2), _
+                                      .HandLocationMid.Y - (Card.CardHeight / 2))
+        End With
+        With Player(4)
+            .HandLocation = New Point(.HandLocationMid.X - (Card.CardWidth / 2), _
+                                      .HandLocationMid.Y - (Card.CardHeight + cardOffsetY * (.TotalCards - 1)) / 2)
+        End With
+    End Sub
+#End Region
+
+#Region "Main Menu Methods"
+    Private Sub NewGameToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles NewGameToolStripMenuItem.Click
+        NewGame()
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ExitToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+#End Region
 
 End Class
---
+++ Blitz2/Player.vb
@@ -0,0 +1,149 @@
+﻿'    Blitz, a simple draw and discard game, suitable for players of all ages.
+'    Copyright (C) 2009-2010  Ryan Skeldon <psykad@gmail.com>
+'
+'    This program is free software; you can redistribute it and/or modify
+'    it under the terms of the GNU General Public License as published by
+'    the Free Software Foundation; either version 2 of the License, or
+'    (at your option) any later version.
+'
+'    This program is distributed in the hope that it will be useful,
+'    but WITHOUT ANY WARRANTY; without even the implied warranty of
+'    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'    GNU General Public License for more details.
+'
+'    You should have received a copy of the GNU General Public License
+'    along with this program; if not, write to the Free Software
+'    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+Option Explicit On
+
+Namespace Objects
+    Public Class Player
+
+#Region "Fields"
+        Private _name As String
+        Public Property Name() As String
+            Get
+                Return _name
+            End Get
+            Set(ByVal value As String)
+                _name = value
+            End Set
+        End Property
+
+        Private _mode As Modes
+        Public Property Mode() As Modes
+            Get
+                Return _mode
+            End Get
+            Set(ByVal value As Modes)
+                _mode = value
+            End Set
+        End Property
+
+        Private _hand(3) As Card
+        Public Property Hand() As Card()
+            Get
+                Return _hand
+            End Get
+            Set(ByVal value As Card())
+                _hand = value
+            End Set
+        End Property
+
+        Private _handLocation As Point
+        Public Property HandLocation() As Point
+            Get
+                Return _handLocation
+            End Get
+            Set(ByVal value As Point)
+                _handLocation = value
+            End Set
+        End Property
+
+        Private _handLocationMid As Point
+        Public Property HandLocationMid() As Point
+            Get
+                Return _handLocationMid
+            End Get
+            Set(ByVal value As Point)
+                _handLocationMid = value
+            End Set
+        End Property
+
+        Private _totalCards As Byte
+        Public Property TotalCards()
+            Get
+                Return _totalCards
+            End Get
+            Set(ByVal value)
+                _totalCards = value
+            End Set
+        End Property
+
+        Private _tokens As Byte
+        Public Property Tokens() As Byte
+            Get
+                Return _tokens
+            End Get
+            Set(ByVal value As Byte)
+                _tokens = value
+            End Set
+        End Property
+
+        Private _inGame As Boolean
+        Public Property InGame() As Boolean
+            Get
+                Return _inGame
+            End Get
+            Set(ByVal value As Boolean)
+                _inGame = value
+            End Set
+        End Property
+
+        Private _flagged As Boolean
+        Public Property Flagged() As Boolean
+            Get
+                Return _flagged
+            End Get
+            Set(ByVal value As Boolean)
+                _flagged = value
+            End Set
+        End Property
+
+        Public Enum Modes As Byte
+            Computer = 0
+            Human = 1
+        End Enum
+#End Region
+
+#Region "Methods"
+        Public Sub New(ByVal mode As Modes)
+            Me.Mode = mode
+
+            CreateNewHand()
+        End Sub
+
+        Public Sub CreateNewHand()
+            For i As Byte = 0 To 3
+                Hand(i) = New Card(Nothing)
+            Next
+
+            TotalCards = 0
+        End Sub
+
+        Public Sub AddCard(ByVal card As Byte)
+            If TotalCards = 4 Then Exit Sub
+
+            Hand(FreeCard).Position = card
+            Hand(FreeCard).InUse = True
+            TotalCards += 1
+        End Sub
+
+        Private Function FreeCard() As Byte
+            For FreeCard = 0 To UBound(Hand)
+                If Not Hand(FreeCard).InUse Then Return FreeCard
+            Next
+        End Function
+#End Region
+    End Class
+End Namespace
